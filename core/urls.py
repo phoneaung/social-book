1697615d2c116be68491c15c2f9815f336017fa7
Phone Aung <phonexaung@gmail.com>
2023-04-12 10:22:35 +0630

    Add likepost function

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('upload',views.upload, name='upload'),
+    path('like-post', views.like_post, name='like-post'),
     path('settings', views.settings, name='settings'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
