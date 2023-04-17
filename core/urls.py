3bb02cb06e442e01fd5aaaa30a57f348854b06cf
Phone Aung <phonexaung@gmail.com>
2023-04-17 17:14:16 +0630

    add search function

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('upload',views.upload, name='upload'),
     path('follow', views.follow, name='follow'),
+    path('search', views.search, name='search'),
     path('profile/<str:pk>', views.profile, name='profile'),
     path('like-post', views.like_post, name='like-post'),
     path('settings', views.settings, name='settings'),
